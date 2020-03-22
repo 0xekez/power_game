5978d1e2197a051add91c675aefd5e43a997dc43
julianicholson <julianicholson@berkeley.edu>
2020-03-22 16:43:00 -0700

    Update julia.py

--- julia.py
+++ julia.py
@@ -9,14 +9,38 @@ class Julia(Player):
         # variables in order.
         super().__init__("julia")
         self.past_clearing_prices = []
+        self.past_ccps = []
 
     def bid(self, demand):
         my_bid = []
+        ccp = self.competative_clearing_price(demand)
         for plant in self.power_plants:
-            # Just try to sell our power at a rate that gives us a
-            # decent profit.
-            my_bid.append([plant, plant.price_per_kwh * 1.50])
+            #bids competative clearing price for each of my plants
+            my_bid.append([plant, ccp])
         return my_bid
 
     def receive_update(self, update):
-        self.past_clearing_prices += update
+        self.past_clearing_prices += [update]
+
+    def competative_clearing_price(self, demand):
+        all_plants = []
+        for group in plant_groups:
+            all_plants += [plant for plant in group]
+        demand_left, ccp = demand, 0
+        while demand_left > 0:
+            if not all_plants:
+                return ccp
+            lowest_bid, lowest_bidding_index = 100000, 0
+            for i in range(0, len(all_plants)):
+                plant = all_plants[i]
+                if plant.price_per_kwh < lowest_bid:
+                    lowest_bid, lowest_bidding_index = plant.price_per_kwh, i
+            lowest_bidder = all_plants[lowest_bidding_index]
+            if lowest_bidder.capacity <= demand_left:
+                ccp += lowest_bid*lowest_bidder.capacity
+                demand_left -= lowest_bidder.capacity
+                all_plants = all_plants[:lowest_bidding_index]+all_plants[lowest_bidding_index+1:]
+            else:
+                ccp += lowest_bid*demand_left
+                demand_left = 0
+        return ccp
